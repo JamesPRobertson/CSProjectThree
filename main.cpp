780247656466e37cb25626f2971311358e2fc15e
JamesRobRob <Jamespr.0312@gmail.com>
2019-09-25 22:20:09 -0500

    Working better

--- main.cpp
+++ main.cpp
@@ -9,27 +9,49 @@ int main() {
 	std::string input = "";
 	std::cout << "Please enter a postfix expression as a string" << std::endl;
 	std::getline(std::cin,input);
-	std:: cout << "\n" << postFix(input);
+	std:: cout <<  postFix(input);
 }
 
 int postFix(std::string inputString){
 	std::stack<int> *theStack = new std::stack<int>();
 
 	for(int i = 0; i < inputString.size(); i++){
-		if(inputString[i] == '+' ||
-		   inputString[i] == '-' ||
-		   inputString[i] == '*' ||
-		   inputString[i] == '/'){
+		switch(inputString[i]){
+			default:
+				theStack->push((char)inputString[i] - '0');
+				break;
+			case '*': {
+				int firstMulti = theStack->top();
+				theStack->pop();
+				int secondMulti = theStack->top();
+				theStack->pop();
 
-			int foo = theStack->top();
-			theStack->pop();
-			int bar = theStack->top();
-			theStack->pop();
+				return firstMulti * secondMulti;
+			}
+			case '/': {
+				int firstDiv = theStack->top();
+				theStack->pop();
+				int secondDiv = theStack->top();
+				theStack->pop();
 
-			
-		}
-		else{
-			theStack->push(std::atoi(&inputString[i]));
+				return firstDiv / secondDiv;
+			}
+			case '+': {
+				int firstAdd = theStack->top();
+				theStack->pop();
+				int secondAdd = theStack->top();
+				theStack->pop();
+
+				return firstAdd + secondAdd;
+			}
+			case '-': {
+				int firstMin = theStack->top();
+				theStack->pop();
+				int secondMin = theStack->top();
+				theStack->pop();
+
+				return firstMin  * secondMin;
+			}
 		}
 	}
 }
